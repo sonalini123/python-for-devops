1945de45e9d1306fdfa0e3818c3ffe2b6105a6df
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-19 16:04:33 -0400

    Update test.py

--- Day-04/test.py
+++ Day-04/test.py
@@ -1,7 +1,7 @@
 import sys
 def add(num1,num2):
-  x = num1+num2
-  return x
+  add = num1+num2
+  return add
 
 def sub(num1,num2):
   y = num1+num2
@@ -11,7 +11,7 @@ num1 = float(sys.argv[1])
 operation = sys.argv[2]
 num2 = float(sys.argv[3])
 
-if operation =="add":
-  output = addition(num1,num2)
+if operation == "add":
+  output = add(num1,num2)
   print(output)
 
