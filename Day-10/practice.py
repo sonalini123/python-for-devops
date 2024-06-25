cf3cb17fdad1367c5f831ceb93b4fe704257f47d
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-25 19:22:34 -0400

    Create practice.py

---
+++ Day-10/practice.py
@@ -0,0 +1,10 @@
+"""
+import sys
+num1 = sys.argv[1]
+num2 = sys.argv[2]
+sum_of_nos = (num1+num2)
+print(sum_of_nos)
+"""
+num1 = int(input("enter first number:"  ))
+num2 = int(input("enter second number:" ))
+print(num1 + num2)
