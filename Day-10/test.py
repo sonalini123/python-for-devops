677a1948f0106d648b579d1deff436167c227a43
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-25 18:55:51 -0400

    Update test.py

--- Day-10/test.py
+++ Day-10/test.py
@@ -1,2 +1,24 @@
+"""
+import sys
+number = sys.argv[1]
+print(number)
+"""
+"""
 number = input("provide any number: ")
 print(number)
+"""
+ 
+import os
+folder_names = input("provide folder names: ").split()
+for i in folder_names:
+   try:  
+     files = os.listdir(i)
+   except FileNotFoundError:
+    print("provide valid folder name: " + i )
+    break
+   except PermissionError:
+    print("no access to this folder:" + i )
+    break
+   print("====listing files for the folder " + i)
+   for file in files:
+      print(file)
