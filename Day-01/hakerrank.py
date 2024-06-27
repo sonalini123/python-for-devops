8fe9ae6e4efbca4a7e6a57b2ebd4b5b73fd7599c
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-27 07:04:33 -0400

    Update hakerrank.py

--- Day-01/hakerrank.py
+++ Day-01/hakerrank.py
@@ -3,3 +3,13 @@ n = int(input("enter any number: "))
 number_list = list(range(n))
 for i in range(0,n):
   print(i*i)  # Output: [0, 1, 2]
+  
+# Given a year, determine whether it is a leap year
+
+def is_leap_year(year):
+  if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0 ):
+    return True
+  else:
+        return False
+year = int(input("enter year: "))
+print(is_leap_year(year))       
