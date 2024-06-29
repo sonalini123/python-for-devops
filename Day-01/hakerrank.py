7ae964fd583776df45bfb37f94b786745779e697
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-28 21:00:24 -0400

    Update hakerrank.py

--- Day-01/hakerrank.py
+++ Day-01/hakerrank.py
@@ -13,3 +13,18 @@ def is_leap_year(year):
         return False
 year = int(input("enter year: "))
 print(is_leap_year(year))       
+
+#Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+#Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+student_marks = []
+n = int(input("enter the number of students: "))
+
+for _ in range(n):
+  name = input()
+  marks = int(input())
+  student_marks.append([name, marks])
+  sorted_student_marks = sorted(student_marks, key=lambda x: x[1], reverse=True)
+  lowest_marks_student = sorted_student_marks[-1]
+  print(student_marks[0])
+  
