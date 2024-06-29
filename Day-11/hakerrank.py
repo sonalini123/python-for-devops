572e5db00c75560032cd367a6de8c781085d53bd
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-29 10:51:17 -0400

    Update hakerrank.py

--- Day-11/hakerrank.py
+++ Day-11/hakerrank.py
@@ -1,13 +1,16 @@
+#The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.Example
+#The query_name is 'beta'. beta's average score is .
 my_dict = {}
 n = int(input("enter the number of entries: "))
 for _ in range (n):
-    name = input("enter the name: ")
-    marks = int(input("enter the marks: "))
-    my_dict [name] = marks
+    name, *line = input().split()
+    scores = list(map(float, line))
+    my_dict [name] = scores
     print(my_dict)
-key_to_print = input("enter the key you want to print: ")
-
+key_to_print = input("enter the key you want to print the average for: ")
 if key_to_print in my_dict:
-    print(key_to_print)
+    average_marks = sum(scores) / len(scores)
+    print(f"average of marks for '{key_to_print}' is: ", average_marks)
 else:
         print("key value not present in my_dict")
+
