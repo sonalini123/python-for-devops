424adb87ad3751b98481f634da39258f2eddb772
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-06-30 06:36:03 -0400

    Update hakerrank.py

--- Day-11/hakerrank.py
+++ Day-11/hakerrank.py
@@ -14,3 +14,18 @@ if key_to_print in my_dict:
 else:
         print("key value not present in my_dict")
 
+#Students of District College have a subscription to English and French newspapers. Some students have subscribed to only the English newspaper, some have subscribed to only the French newspaper, and some have subscribed to both newspapers.
+
+#You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to only English newspapers.
+
+Eng_no = int(input("enter the number of students who have subscribed to Eng newspaper: "))
+Eng_sub = set(map(int,input().split()))
+
+Frn_no = int(input("enter the number of students who have subscribed to Frn newspaper: "))
+Frn_sub = set(map(int,input().split()))
+
+difference_EngFrn = Eng_sub.difference(Frn_sub)
+#difference_FrnEng_operator = set_Frn - set_Eng
+
+print("no of students subscribed for Eng newspaper: ", (len(difference_EngFrn)))
+#print("Difference (set_Frn - set_Eng) using - operator:", difference_FrnEng_operator)
