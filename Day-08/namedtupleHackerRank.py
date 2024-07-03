1f4ad178bc79f2c9e907c0e69e13d4159f66d134
sonalini123 <80326531+sonalini123@users.noreply.github.com>
2024-07-03 07:28:06 -0400

    Update namedtupleHackerRank.py

--- Day-08/namedtupleHackerRank.py
+++ Day-08/namedtupleHackerRank.py
@@ -1,1 +1,15 @@
-
+from collections import namedtuple
+N = int(input("enter total number of students: "))
+Student = namedtuple('Student',['ID','Marks','Name','Class'])
+students = []
+for i in range(N):
+    print(f"enter the student info {i+1}:")
+    ID = int(input())
+    Marks = int(input())
+    Name = input()
+    Class = int(input())
+student = Student(ID,Marks,Name,Class)
+students.append(student)
+for Marks in (students[]):
+    average = sum(Marks)/len(Marks)
+    print(average)
